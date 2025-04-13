f80a96ff8d3c53ab159778845a93fb7fcf9af060
Sarah <louise562@aol.com>
2025-04-13 15:41:35 -0500

    Finished app

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,99 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useEffect, useState } from 'react'
+import headerBanner from './assets/header-banner.png'
+
+const apiKey = import.meta.env.VITE_GIPHY_API_KEY;
 
 function App() {
-  const [count, setCount] = useState(0)
+
+  const [images, setImages] = useState([]);
+  
+  useEffect(() => {
+    const getImages = async () => {
+      const res = await fetch(`https://api.giphy.com/v1/gifs/search?api_key=${apiKey}&q=schitts&limit=10`).catch(err => console.log(err));
+      const dataRes = await res.json();
+
+      const addProps = dataRes.data.map((imgObj) => ({...imgObj, clicked:false}));
+      setImages(addProps);
+    }
+    
+    getImages();
+  }, []);
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+        <div className="header">
+          <img src={headerBanner} />
+        </div>
+        <Game imageList={images} />
+    </>
+  )
+}
+
+
+function Game({ imageList }){
+  const [images, setImages] = useState(imageList);
+  const [score, setScore] = useState(0);
+  const [best, setBest] = useState(0);
+
+
+  useEffect(() => {
+    if (imageList.length > 0) {
+      setImages(imageList);
+    }
+  }, [imageList]);
+
+  useEffect(() => {
+    if (score > best) setBest(score);
+  }, [score]);
+
+  const shuffleArray = (array) => {
+    const shuffledArr = [];
+    const usedIndexes = [];
+
+    while (shuffledArr.length < array.length) {
+      const randomIndex = Math.floor(Math.random() * array.length);
+      if (!usedIndexes.includes(randomIndex)) {
+        shuffledArr.push(array[randomIndex]);
+        usedIndexes.push(randomIndex);
+      }
+    }
+
+    return shuffledArr
+  }
+
+  const handleImageClick = (e) => {
+    const getImageClicked = images.filter((image) => (e.target.id === image.id))[0];
+    let updatedImageList = [];
+    let shuffledImageList = [];
+
+    if (getImageClicked.clicked) {
+      //losing conditions
+      setScore(0);
+      updatedImageList = images.map((image) => ({...image, clicked:false}))
+      shuffledImageList = shuffleArray(updatedImageList);
+      setImages(shuffledImageList);
+    } else {
+      setScore(score + 1);
+      updatedImageList = images.map((image) => (e.target.id === image.id ? { ...image, clicked: true } : image));
+      shuffledImageList = shuffleArray(updatedImageList);
+      setImages(shuffledImageList);
+    }
+  }
+
+  return (
+    <>
+      <div className="directions">        
+        <p>Get points by clicking on an image but don't click on any more than once!</p>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
+
+      <div className="score">
+        <p><span style={{ fontWeight: "bold" }}>Score:</span> {score}</p>
+        <p><span style={{ fontWeight: "bold" }}>Best Score:</span> {best}</p>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+
+      <ul className="image-list">
+        {images.map((image) => (<li> <img onClick={handleImageClick} id={image.id} src={image.images.original.url}></img> </li>))}
+      </ul>
     </>
   )
 }
